e2abeda185c34bcd29d066ed18feeb292ea2d1f2
DyusembaevaAzira <83331431+DyusembaevaAzira@users.noreply.github.com>
2023-05-25 18:10:26 +0600

    Create Lesson.kt

---
+++ src/commonMain/kotlin/ru.altmanea.webapp/data/Lesson.kt
@@ -0,0 +1,33 @@
+package ru.altmanea.webapp.data
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import ru.altmanea.webapp.common.ItemId
+
+
+@Serializable
+class Lesson(
+    val name: String,
+    val teacher: TeacherId,
+    val students: Array<GradeInfo> = emptyArray()
+) {
+    fun addStudent(studentId: StudentId) =
+        Lesson(
+            name,
+            teacher,
+            students + GradeInfo (studentId, null)
+        )
+}
+
+@Serializable
+class GradeInfo(
+    val studentId: StudentId,
+    val grade: Grade?
+)
+
+typealias LessonId = ItemId
+
+val Lesson.json
+    get() = Json.encodeToString(this)
+
