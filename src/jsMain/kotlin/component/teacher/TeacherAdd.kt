398400467f5b503bf56a747e0c3a602dd085b6b2
DyusembaevaAzira <83331431+DyusembaevaAzira@users.noreply.github.com>
2023-05-25 18:27:38 +0600

    Create TeacherAdd.kt

---
+++ src/jsMain/kotlin/component/teacher/TeacherAdd.kt
@@ -0,0 +1,33 @@
+package component.teacher
+
+import component.template.EditAddProps
+import react.FC
+import react.dom.html.ReactHTML
+import react.useState
+import ru.altmanea.webapp.data.Teacher
+import web.html.InputType
+
+val TeacherAdd = FC<EditAddProps<Teacher>>{props ->
+    var firstname by useState("")
+    var surname by useState("")
+    ReactHTML.span {
+        ReactHTML.input {
+            type = InputType.text
+            value = firstname
+            onChange = { firstname = it.target.value }
+        }
+        ReactHTML.input {
+            type = InputType.text
+            value = surname
+            onChange = { surname = it.target.value }
+        }
+    }
+    ReactHTML.button {
+        +"✓"
+        onClick = {
+            props.saveElement(Teacher(firstname, surname))
+            firstname = ""
+            surname = ""
+        }
+    }
+}
